5fa2b7bd2ea502bcf47b221378fcb6828ca686b4
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-17 14:34:53 -0500

    1

--- todo_list_manager/ajax/addTask.php
+++ todo_list_manager/ajax/addTask.php
@@ -1,15 +1,16 @@
 <?php
 require_once('../includes/db.php'); // The mysql database connection script require('dbconnection.php');
-if(isset($_GET['task'])){
-$task = $_GET['task'];
-$status = "0";
-$created = time();
+if(isset($_GET['task']))
+{
+  $task = $_GET['task'];
+  $status = "0";
+  $created = time();
 
-$query="INSERT INTO tasks(task,status,created_at)  VALUES ('$task', '$status', '$created')";
-$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+  $query="INSERT INTO tasks(task,status,created_at)  VALUES ('$task', '$status', '$created')";
+  $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
-$result = $mysqli->affected_rows;
+  $result = $mysqli->affected_rows;
 
-echo $json_response = json_encode($result);
+  echo $json_response = json_encode($result);
 }
 ?>
--- todo_list_manager/ajax/deleteTask.php
+++ todo_list_manager/ajax/deleteTask.php
@@ -1,13 +1,14 @@
 <?php
 require_once('../includes/db.php'); // The mysql database connection script
-if(isset($_GET['taskID'])){
-$taskID = $_GET['taskID'];
+if(isset($_GET['taskID']))
+{
+  $taskID = $_GET['taskID'];
 
-$query="delete from tasks where id='$taskID'";
-$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+  $query="delete from tasks where id='$taskID'";
+  $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
-$result = $mysqli->affected_rows;
+  $result = $mysqli->affected_rows;
 
-echo $json_response = json_encode($result);
+  echo $json_response = json_encode($result);
 }
 ?>
