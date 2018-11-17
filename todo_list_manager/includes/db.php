e2cc7ba2c37eec5558e8c7cc84af63518794631c
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-17 14:45:05 -0500

    1

--- todo_list_manager/includes/db.php
+++ todo_list_manager/includes/db.php
@@ -2,7 +2,7 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'roman';
 $DB_PASS = 'southhills#';
-$DB_NAME = 'angularcode_task';
+$DB_NAME = 'roman';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
 if ($conn->connect_error)
