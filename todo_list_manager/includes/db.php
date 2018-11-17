6c8dd22c71dfc5b34a046af503e1efc2f949b9cc
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-17 01:27:03 -0500

    1

--- todo_list_manager/includes/db.php
+++ todo_list_manager/includes/db.php
@@ -4,4 +4,9 @@ $DB_USER = 'roman';
 $DB_PASS = 'southhills#';
 $DB_NAME = 'angularcode_task';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+if ($conn->connect_error)
+{
+  echo "CANT TOUCH THIS";
+}
 ?>
