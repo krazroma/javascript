4777027fa9386e7b2790ea53bc89ec191ec9824b
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-20 14:37:33 -0500

    1

--- v2/work.js
+++ v2/work.js
@@ -1,24 +1,17 @@
 var app = angular.module("app", ["xeditable", "ngMockE2E"]);
 
-app.run(function(editableOptions) {
-  editableOptions.theme = 'bs3';
-});
+
 
 app.controller('Ctrl', function($scope, $filter, $http) {
  $scope.users = [
     {id: 1, name: 'awesome user1'}
-  ]; 
+  ];
 
 
   $scope.saveUser = function(data, id) {
     //$scope.user not updated yet
     angular.extend(data, {id: id});
     return $http.post('/saveUser', data);
-  };
-
-  // remove user
-  $scope.removeUser = function(index) {
-    $scope.users.splice(index, 1);
   };
 
   // add user
@@ -36,7 +29,7 @@ app.run(function($httpBackend) {
   $httpBackend.whenGET('/groups').respond([
     {id: 1, text: 'user'}
   ]);
-    
+
   $httpBackend.whenPOST(/\/saveUser/).respond(function(method, url, data) {
     data = angular.fromJson(data);
     return [200, {status: 'ok'}];
