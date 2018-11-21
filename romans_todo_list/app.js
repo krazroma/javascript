439fe14da04cbfbf8646e6357c44806378ba8988
krazroma <42806048+krazroma@users.noreply.github.com>
2018-11-20 21:21:08 -0500

    1

--- romans_todo_list/app.js
+++ romans_todo_list/app.js
@@ -48,33 +48,33 @@ app.controller('shopController', function($scope, $http) {
   //         $scope.model.selected = {};
   //     };
 
-      $scope.model = {
-          contacts: [{
-              id: 1,
-              name: "Ben"
-          }],
-          selected: {}
-      };
-
-      // gets the template to ng-include for a table row / item
-      $scope.getTemplate = function (contact) {
-          if (contact.id === $scope.model.selected.id) return 'edit';
-          else return 'display';
-      };
-
-      $scope.editContact = function (contact) {
-          $scope.model.selected = angular.copy(contact);
-      };
-
-      $scope.saveContact = function (idx) {
-          console.log("Saving contact");
-          $scope.model.contacts[idx] = angular.copy($scope.model.selected);
-          $scope.reset();
-      };
-
-      $scope.reset = function () {
-          $scope.model.selected = {};
-      };
+      // $scope.model = {
+      //     contacts: [{
+      //         id: 1,
+      //         name: "Ben"
+      //     }],
+      //     selected: {}
+      // };
+      //
+      // // gets the template to ng-include for a table row / item
+      // $scope.getTemplate = function (contact) {
+      //     if (contact.id === $scope.model.selected.id) return 'edit';
+      //     else return 'display';
+      // };
+      //
+      // $scope.editContact = function (contact) {
+      //     $scope.model.selected = angular.copy(contact);
+      // };
+      //
+      // $scope.saveContact = function (idx) {
+      //     console.log("Saving contact");
+      //     $scope.model.contacts[idx] = angular.copy($scope.model.selected);
+      //     $scope.reset();
+      // };
+      //
+      // $scope.reset = function () {
+      //     $scope.model.selected = {};
+      // };
 
 
 
